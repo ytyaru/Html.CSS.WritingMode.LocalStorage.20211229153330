f5800f8851be332d6defa53ede54e52a1773d46f
ytyaru <ytyaru@outlook.jp>
2022-01-07 14:17:28 +0900

    0.25 フォントと画面のサイズを計算する（段組余白を考慮）

---
+++ docs/0.25/js/main.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    initFullScreen();
+    initWritingMode();
+    initColumns();
+    initLineOfChars();
+    initLineHeight();
+    initLetterSpacing();
+    console.log("DOMContentLoaded");
+    console.log(`devicePixelRatio = ${window.devicePixelRatio}`);
+});
+window.addEventListener('load', (event) => {
+    console.log("load");
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log("beforeunload");
+    saveFullScreen();
+    saveColumns();
+    saveWritingMode();
+    saveLineOfChars();
+    saveLineHeight();
+    saveLetterSpacing();
+});
+window.addEventListener("orientationchange", function () { // 画面向きに応じて最大字数／行を変更する
+    console.log("orientationchange");
+    setMaxColumns(); // columns.js
+    setMaxLineOfChars(); // resize.js
+    setFontSizePixel(document.querySelector('#columns').value, document.querySelector('#writing-mode').value, document.querySelector('#line-of-chars').value, document.querySelector('#letter-spacing').value); // resize.js
+//    setLineOfChars(); // resize.js
+    calcScreenSize(document.querySelector('#writing-mode').value, document.querySelector('#columns').value); // resize.js
+});
+window.addEventListener("resize", function (e) { // 全画面やリサイズ時に字／行の値を再計算する
+    console.log("resize");
+    /*
+    setMaxLineOfChars(); // resize.js
+    setLineOfChars();
+    setMaxColumns();
+    */
+    setMaxColumns(); // columns.js
+    setMaxLineOfChars(); // resize.js
+    setFontSizePixel(document.querySelector('#columns').value, document.querySelector('#writing-mode').value, document.querySelector('#line-of-chars').value, document.querySelector('#letter-spacing').value); // resize.js
+//    setLineOfChars(); // resize.js
+
+});
+
