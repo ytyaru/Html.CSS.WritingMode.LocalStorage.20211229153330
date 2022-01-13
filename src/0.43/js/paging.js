6474facd26b6299084a5eb97fac3227d4db6173e
ytyaru <ytyaru@outlook.jp>
2022-01-13 19:33:03 +0900

    0.43 余白（ページ数計算できなかった。columnuではダメ）

---
+++ src/0.43/js/paging.js
@@ -0,0 +1,207 @@
+function nextPage() { movePageRelative(1); calcPage(); }
+function prevPage() { movePageRelative(-1); calcPage();  }
+function firstPage() { movePageAbsolute(0); calcPage();  }
+function lastPage() { movePageAbsolute(-1); calcPage();  }
+function movePage(page) { movePageAbsolute(page); calcPage();  }
+function movePageRelative(increment=1) { // incrementが正数なら進む。負数なら戻る。0なら何もしない。
+    const [ALL_PAGE, NOW_PAGE] = calcPage();
+    if (increment < 1 && 0 === NOW_PAGE) { return; } // 先頭ページ以上は進まない（微妙な余白があるが移動させない）
+    if (0 < increment && (ALL_PAGE - 1) === NOW_PAGE) { return; } // 末尾ページ以上は進まない（微妙な余白があるが移動させない）
+    function cssF(key,q=':root') { return parseFloat(getComputedStyle(document.querySelector(q)).getPropertyValue(key)); }
+    const IS_VERTICAL = ('vertical-rl' === document.querySelector('#writing-mode').value);
+    const COL_GAP_EM = cssF('--column-gap-em');
+    const FONT_SIZE_PX = cssF('--font-size-px');
+    const COL_GAP_PX = FONT_SIZE_PX * COL_GAP_EM;
+    const COL_RULE_WIDTH_PX = cssF('--column-rule-width-px');
+    const COLUMN_COUNT = cssF('--column-count');
+//    const PAGE_PX = (cssF(((IS_VERTICAL) ? 'height' : 'width'), 'body') + COL_GAP_PX + (COL_RULE_WIDTH_PX * (0<increment) ? 1 : -1) + ((COL_RULE_WIDTH_PX/2)*((0<increment) ? -1 : 1 ))) * increment;
+//    const PAGE_PX = (cssF(((IS_VERTICAL) ? 'height' : 'width'), 'body') + COL_GAP_PX) * increment * COLUMN_COUNT;
+//    const PAGE_PX = (cssF(((IS_VERTICAL) ? 'height' : 'width'), 'body') + COL_GAP_PX + (COL_RULE_WIDTH_PX * (0<increment) ? 1 : -1) + ((COL_RULE_WIDTH_PX/2)*((0<increment) ? -1 : 1 ))) * increment * COLUMN_COUNT;
+    const MAIN = document.querySelector('main:not([hidden])');
+//    const PAGE_PX = (IS_VERTICAL) ? MAIN.clientHeight : MAIN.clientWidth; // １行の表示領域
+//    const PAGE_PX = ((IS_VERTICAL) ? MAIN.clientHeight : MAIN.clientWidth) + COL_GAP_PX; // １行の表示領域
+//    const PAGE_PX = (((IS_VERTICAL) ? MAIN.clientHeight : MAIN.clientWidth) + COL_GAP_PX ) * increment; // １行の表示領域
+//    const PAGE_PX = ((((IS_VERTICAL) ? MAIN.clientHeight : MAIN.clientWidth)) * increment) + (COL_GAP_PX*(increment)) + (((0<increment) ? 0 : 1) * COLUMN_COUNT); // １行の表示領域
+
+//    const PAGE_PX = ((((IS_VERTICAL) ? MAIN.clientHeight : MAIN.clientWidth) + COL_GAP_PX) * increment) + ((1<COLUMN_COUNT) ? 0.5 : 0) + (((0<increment) ? 0 : 1) * COLUMN_COUNT); // １行の表示領域
+
+//    const PAGE_PX = cssF(((IS_VERTICAL) ? 'height' : 'width'), 'main:not([hidden])') * increment;
+//    const PAGE_PX = (cssF(((IS_VERTICAL) ? 'height' : 'width'), 'main:not([hidden])') + COL_GAP_PX) * increment;
+    const PAGE_PX = (cssF(((IS_VERTICAL) ? 'height' : 'width'), 'main:not([hidden])') + COL_GAP_PX) * increment + ((0<increment) ? COL_RULE_WIDTH_PX : 0);
+
+//    const PAGE_PX = ((IS_VERTICAL) ? MAIN.scrollHeight : MAIN.scrollWidth) / ALL_PAGE;
+//    const PAGE_PX = (((IS_VERTICAL) ? MAIN.scrollHeight : MAIN.scrollWidth) + (COL_GAP_PX/2)) / ALL_PAGE;
+
+//    const PAGE_PX = (((IS_VERTICAL) ? MAIN.clientHeight : MAIN.clientWidth) + COL_GAP_PX + (COL_RULE_WIDTH_PX * (0<increment) ? 1 : -1) + ((COL_RULE_WIDTH_PX/2)*((0<increment) ? -1 : 1 ))) * increment * COLUMN_COUNT;
+
+//    const PAGE_PX = cssF('column-width', 'main:not([hidden])');
+    console.log(window.screen.height, document.body.clientHeight, MAIN.clientHeight, MAIN.clientWidth, cssF('width', 'main:not([hidden])'));
+    const X_INC = (IS_VERTICAL) ? 0 : PAGE_PX;
+    const Y_INC = (IS_VERTICAL) ? PAGE_PX : 0;
+//    window.scrollBy(X_INC, Y_INC);
+    MAIN.scrollBy(X_INC, Y_INC);
+}
+function movePageAbsolute(page=0) { // pageは整数。0を最初のページとする。負数なら最後のページから数えた値。
+    const IS_VERTICAL = ('vertical-rl' === document.querySelector('#writing-mode').value);
+    const MAIN = document.querySelector('main:not([hidden])');
+    function cssF(key,q=':root') { return parseFloat(getComputedStyle(document.querySelector(q)).getPropertyValue(key)); }
+    function calcPagePx() {
+//        const PAGE_PX = parseFloat(getComputedStyle(document.querySelector('body')).getPropertyValue((IS_VERTICAL) ? 'height' : 'width'));
+//        const PAGE_PX = (IS_VERTICAL) ? MAIN.clientHeight : MAIN.clientWidth; // １行の表示領域
+        const PAGE_PX = cssF(((IS_VERTICAL) ? 'height' : 'width'), 'main:not([hidden])');
+        const COL_GAP_EM = parseFloat(getComputedStyle(document.querySelector(':root')).getPropertyValue('--column-gap-em'));
+        const FONT_SIZE_PX = parseFloat(getComputedStyle(document.querySelector(':root')).getPropertyValue('--font-size-px'));
+        const COL_GAP_PX = FONT_SIZE_PX * COL_GAP_EM;
+        const COL_RULE_WIDTH_PX = parseFloat(getComputedStyle(document.querySelector(':root')).getPropertyValue('--column-rule-width-px'));
+        const COLUMN_COUNT = cssF('--column-count');
+        const [ALL_PAGE, NOW_PAGE] = calcPage();
+        if (0 <= page) {
+            const increment = NOW_PAGE - page;
+            return (PAGE_PX + COL_GAP_PX + (COL_RULE_WIDTH_PX * (0<increment) ? 1 : -1) + ((0<increment) ? 0 : 1)) * page * COLUMN_COUNT;
+        }
+        else {
+            const TARGET_PAGE = ALL_PAGE + page;
+            const increment = TARGET_PAGE - NOW_PAGE;
+            return (PAGE_PX + COL_GAP_PX + (COL_RULE_WIDTH_PX * (0<increment) ? 1 : -1) + ((0<increment) ? 0 : 1)) * (TARGET_PAGE * COLUMN_COUNT);
+        }
+    }
+    const PAGE_PX = calcPagePx();
+    console.log(`*********PAGE_PX:${PAGE_PX}`)
+    const X_INC = (IS_VERTICAL) ? 0 : PAGE_PX;
+    const Y_INC = (IS_VERTICAL) ? PAGE_PX : 0;
+//    window.scrollTo(X_INC, Y_INC);
+    MAIN.scrollTo(X_INC, Y_INC);
+}
+function calcPage() {
+    const IS_VERTICAL = ('vertical-rl' === document.querySelector('#writing-mode').value);
+//    const PAGE_PX = parseFloat(getComputedStyle(document.querySelector('body')).getPropertyValue((IS_VERTICAL) ? 'height' : 'width'));
+    function cssF(key,q=':root') { return parseFloat(getComputedStyle(document.querySelector(q)).getPropertyValue(key)); }
+    const COL_GAP_EM = cssF('--column-gap-em');
+    const FONT_SIZE_PX = cssF('--font-size-px');
+    const COL_GAP_PX = FONT_SIZE_PX * COL_GAP_EM;
+//    const COL_RULE_WIDTH_PX = cssF('--column-rule-width-px');
+//    const PAGE_PX = (cssF(((IS_VERTICAL) ? 'height' : 'width'), 'body')) + (COL_GAP_PX / 2);
+//    const COLUMN_COUNT = cssF('--column-count');
+//    const PAGE_PX = (cssF(((IS_VERTICAL) ? 'height' : 'width'), 'body')) + ((COL_GAP_PX / 2)*COLUMN_COUNT);
+//    const PAGE_PX = (cssF(((IS_VERTICAL) ? 'height' : 'width'), 'body'));
+    const COLUMN_COUNT = cssF('--column-count');
+//    const PAGE_PX = ((cssF(((IS_VERTICAL) ? 'height' : 'width'), 'body')) + (COL_GAP_PX / 2) ) * COLUMN_COUNT;
+//    const PAGE_PX = ((cssF(((IS_VERTICAL) ? 'height' : 'width'), 'body')) + (COL_GAP_PX / (COLUMN_COUNT+1)) ) * COLUMN_COUNT;
+//    const PAGE_PX = (cssF(((IS_VERTICAL) ? 'height' : 'width'), 'body') * COLUMN_COUNT) + (COL_GAP_PX * (COLUMN_COUNT - 1)) ;
+//    const PAGE_PX = (cssF(((IS_VERTICAL) ? 'height' : 'width'), 'body') * COLUMN_COUNT) + (COL_GAP_PX * (COLUMN_COUNT - 1)) ;
+    const MAIN = document.querySelector('main:not([hidden])');
+//    const PAGE_PX = (IS_VERTICAL) ? MAIN.clientHeight : MAIN.clientWidth; // １行の表示領域
+    const PAGE_PX = cssF(((IS_VERTICAL) ? 'height' : 'width'), 'main:not([hidden])');
+//    const PAGE_PX = cssF(((IS_VERTICAL) ? 'height' : 'width'), 'main:not([hidden])') + (COL_GAP_PX/2);
+//    const PAGE_PX = cssF('column-width', 'main:not([hidden])');
+    console.log(`PAGE_PX :${PAGE_PX }`)
+
+//    const COL_GAP_NUM = ((COLUMN_COUNT - 1) * ALL_PAGE_NUM) + (ALL_PAGE_NUM - 1)
+
+//    const ALL_PX = (IS_VERTICAL) ? document.body.scrollHeight : document.body.scrollWidth;
+    const ALL_PX = (IS_VERTICAL) ? MAIN.scrollHeight : MAIN.scrollWidth;
+//    const ALL_PAGE = ALL_PX / PAGE_PX;
+    const ALL_PAGE = ALL_PX / PAGE_PX;
+
+    console.log(`XXXXXXXXXXX:${ALL_PX - (PAGE_PX * ALL_PAGE)}`);
+//    const ALL_PAGE_NUM = ((ALL_PX - ((COLUMN_COUNT - 1) * ALL_PAGE_NUM) + 1) / PAGE_PX) - 1;
+
+//    const pos = getWindowScrollPosition();
+    const pos = getScrollPosition(MAIN)
+    const NOW_PX = (IS_VERTICAL) ? pos.y : pos.x;
+    const NOW_PAGE = (0 === NOW_PX) ? 0 : ALL_PAGE * (NOW_PX / ALL_PX);
+    console.log(`ALL_PAGE:${ALL_PAGE}, NOW_PAGE:${NOW_PAGE}`);
+    console.log(`ALL_PX:${ALL_PX}, PAGE_PX:${PAGE_PX}, NOW_PX:${NOW_PX}`);
+    console.log(`scrollWidth:${document.body.scrollWidth}`);
+    console.log(`ALL_PAGE:${parseInt(ALL_PAGE)}, NOW_PAGE:${parseInt(NOW_PAGE)}`)
+//    console.log(`ALL_PAGE:${parseInt(Math.ceil(ALL_PAGE))}, NOW_PAGE:${parseInt(Math.ceil(NOW_PAGE))}`)
+    return [parseInt(ALL_PAGE), parseInt(NOW_PAGE)];
+//    return [parseInt(Math.ceil(ALL_PAGE)), parseInt(NOW_PAGE)];
+//    return [parseInt(Math.ceil(ALL_PAGE)), parseInt(Math.ceil(NOW_PAGE))];
+}
+function getScrollPosition(e=window) {
+    return {
+        x : typeof e.scrollX !== 'undefined' ? e.scrollX : e.scrollLeft,
+        y : typeof e.scrollY !== 'undefined' ? e.scrollY : e.scrollTop
+    };
+}
+/*
+function getWindowScrollPosition() {
+    return {
+        x : typeof window.scrollX !== 'undefined' ? window.scrollX : (typeof document.documentElement.scrollLeft !== "undefined" ? document.documentElement.scrollLeft : document.body.scrollLeft),
+        y : typeof window.scrollY !== 'undefined' ? window.scrollY : (typeof document.documentElement.scrollTop  !== "undefined" ? document.documentElement.scrollTop  : document.body.scrollTop)
+    };
+};
+*/
+
+function initPaging() {
+    // window -> document.body に変更することで dialog.showModal() 時にイベント発火抑制できるかと思ったが、できなかった。
+    window.addEventListener('touchstart', (event) => {
+        console.log('touchstart', event);
+        const setting = document.querySelector('#setting');
+        if (setting.open) { if(!event.target.closest('#setting > form[method="dialog"]')) {setting.close();} return; }
+        runScreenAreaRole(event.touches[0].X, event.touches[0].Y);
+    });
+    window.addEventListener('click', (event) => {
+        const setting = document.querySelector('#setting');
+        if (setting.open) { if(!event.target.closest('#setting > form[method="dialog"]')) {setting.close();} return; }
+        runScreenAreaRole(event.clientX, event.clientY);
+    });
+    window.addEventListener('mousemove', (event) => {
+        if (document.querySelector('#setting').open) { return; }
+        // body内に要素がない領域は反応しない！　末尾ページは下端までない箇所では反応しない！
+        document.body.style.cursor = getCursorScreenAreaRole(event.clientX, event.clientY);
+    });
+    window.addEventListener("keydown", event => {
+        if (document.querySelector('#setting').open) { return; }
+        const IS_VERTICAL = ('vertical-rl' === document.querySelector('#writing-mode').value);
+        console.log(`keydown event.key:${event.key}, Shift:${event.shiftKey}`)
+             if (event.key === 'ArrowUp') { event.preventDefault();  }   // menu(index,setting,tools)表示予定
+        else if (event.key === 'ArrowDown') { // setting表示切替
+            const dialog = document.querySelector('#setting');
+            (dialog.open) ? dialog.close() : dialog.showModal();
+            document.body.style.cursor = 'auto';
+            event.preventDefault();
+        }
+        else if (event.key === 'ArrowLeft') {(IS_VERTICAL) ? nextPage() : prevPage(); event.preventDefault();  }
+        else if (event.key === 'ArrowRight') {(IS_VERTICAL) ? prevPage() : nextPage(); event.preventDefault();  }
+        else if (event.key === 'PageUp') {prevPage();event.preventDefault();}
+        else if (event.key === 'PageDown') {nextPage();event.preventDefault();}
+        else if (event.key === 'Home') {firstPage();event.preventDefault();}
+        else if (event.key === 'End') {lastPage();event.preventDefault();}
+        else if (!event.shiftKey && event.key === ' ') {nextPage();event.preventDefault();}
+        else if (event.shiftKey && event.key === ' ') {prevPage();event.preventDefault();}
+        else if (!event.shiftKey && event.key === 'Enter') {nextPage();event.preventDefault();}
+        else if (event.shiftKey && event.key === 'Enter') {prevPage();event.preventDefault();}
+        else if (event.key === 'Backspace') {prevPage();event.preventDefault();}
+        else if (event.key === 'Escape') {;event.preventDefault();} // 本を閉じる予定（本棚に戻る）
+        else {}
+    }, {passive: false});
+    window.addEventListener("keypress", event => {
+        console.log(`keypress event.key:${event.key}`)
+        if (document.querySelector('#setting').open) { if (event.key === 'Escape') {document.querySelector('#setting').close(); event.preventDefault();} return; }
+        const IS_VERTICAL = ('vertical-rl' === document.querySelector('#writing-mode').value);
+        if (event.key === 'n') {nextPage();}
+        else if (event.key === 'p') {prevPage();}
+        else if (event.key === 'f') {firstPage();}
+        else if (event.key === 'l') {lastPage();}
+
+        // 'u' undo 前の移動を取り消す
+        // 'm' move 移動するページを絶対値で指定する
+        else if (event.key === 'm') {
+            const [ALL_PAGE, NOW_PAGE] = calcPage();
+            const page = window.prompt('何ページ目に移動しますか？', `${NOW_PAGE}`)
+            movePage(parseInt(page));
+        }
+        // 'h' here 現在ここは何ページ目かを表示する
+        // 't' time 現在時刻。経過時刻。指定時間、指定時刻までの残り時間を表示する
+        // 'i' index 作品名,編,章,ページ数,著者名(著者他作品リンクへ)
+        // 's' setting
+        else if (event.key === 's') {
+            const dialog = document.querySelector('#setting');
+            (dialog.open) ? dialog.close() : dialog.showModal();
+            document.body.style.cursor = 'auto';
+        }
+        else {}
+    }, {passive: false});
+}
---
+++ src/0.43/js/resize.js
@@ -0,0 +1,59 @@
+function calcFontSizePixel(writingMode, lineOfChars, letterSpacing) { // フォントサイズをピクセル単位で算出する
+    function cssF(key) { return parseFloat(getComputedStyle(document.querySelector(':root')).getPropertyValue(key)); }
+    const IS_VERTICAL = ('vertical-rl' === writingMode);
+    const MAIN = document.querySelector('main:not([hidden])');
+    const LINE_OF_PX = ('vertical-rl' === writingMode) ? MAIN.clientHeight : MAIN.clientWidth; // １行の表示領域
+    const COL_GAP_EM = cssF('--column-gap-em');
+    const fontSizePx = LINE_OF_PX / ((lineOfChars * (1 + letterSpacing)) + (COL_GAP_EM / 2));
+    console.log(`フォントサイズ:${fontSizePx}px`);
+    console.log(`LINE_OF_PX:${LINE_OF_PX}px`);
+    console.log(`letterSpacing:${letterSpacing}em`);
+    console.log(`lineOfChars :${lineOfChars }字`);
+    return fontSizePx;
+}
+function setFontSizePixel(writingMode, lineOfChars, letterSpacing) { // フォントサイズをピクセル単位で算出してHTMLにセットする
+    const FONT_SIZE_PX = calcFontSizePixel(writingMode, lineOfChars, letterSpacing);
+    document.querySelector(':root').style.setProperty('--font-size-px', `${FONT_SIZE_PX}`);
+    document.querySelector(':root').style.setProperty('--letter-spacing-px', `${FONT_SIZE_PX * letterSpacing}`);
+}
+function calcLineOfChars(writingMode) { // 字数／行を算出する（writingMode,フォントサイズpx,行間emから。縦横ボタン押下時）
+    // １行あたりの字数＝(LINE_OF_PX - LetterSpacingPx) / (FontSizePx + LetterSpacingPx)
+    const FontSizePx = parseFloat(getComputedStyle(document.querySelector(':root')).getPropertyValue('--font-size-px'));
+    const LetterSpacingPx = FontSizePx * document.querySelector('#letter-spacing').value;
+//    const LINE_OF_PX = ('vertical-rl' === writingMode) ? document.body.clientHeight : document.body.clientWidth; // １行の表示領域
+    const MAIN = document.querySelector('main:not([hidden])');
+    const LINE_OF_PX = ('vertical-rl' === writingMode) ? MAIN.clientHeight : MAIN.clientWidth; // １行の表示領域
+    const lineOfChars = (LINE_OF_PX - LetterSpacingPx) / (FontSizePx + LetterSpacingPx);
+    console.log(`${lineOfChars}字／行。${window.screen.orientation.type} ${writingMode} ${LINE_OF_PX}px行長 ${FontSizePx}px字大 ${LetterSpacingPx}px字間`);
+    return lineOfChars;
+}
+function setLineOfChars() { // 字数／行を算出してHTMLにセットする
+    const lineOfChars = parseInt(calcLineOfChars(document.querySelector('#writing-mode').value))
+    document.querySelector('#line-of-chars-label').innerHTML = lineOfChars;
+    document.querySelector('#line-of-chars').value = lineOfChars;
+}
+function calcScreenSize(writingMode, columns) { // 画面比率を変える（writingModeと段組数に応じて）
+    const IS_VERTICAL = ('vertical-rl' === writingMode);
+    /*
+    const FULL_ID = (IS_VERTICAL) ? '--width-vw' : '--height-vh' ;
+    const SPLIT_ID = (IS_VERTICAL) ? '--height-vh' : '--width-vw' ;
+    document.querySelector(':root').style.setProperty(FULL_ID, 100);
+    document.querySelector(':root').style.setProperty(SPLIT_ID, 100 / columns); // 段組み数で割る
+    */
+    // inline側が分割される（横書きなら横、縦書きなら縦に段組みされる）
+    const root = document.querySelector(':root');
+    const UNIT_BLOCK = 'v' + ((IS_VERTICAL) ? 'w' : 'h');
+    const UNIT_INLINE = 'v' + ((IS_VERTICAL) ? 'h' : 'w');
+    function getUnitBlock() { 'v' + ((IS_VERTICAL) ? 'w' : 'h'); }
+    function getUnitInline() { 'v' + ((IS_VERTICAL) ? 'h' : 'w'); }
+    root.style.setProperty('--block-size-base', '100' + UNIT_BLOCK);
+    root.style.setProperty('--inline-size-base', `${(100 / columns)}${UNIT_INLINE}`);
+    // 段組余白（WritingModeに応じてセットする。横書きなら横、縦書きなら縦に。それ以外はゼロ）
+    const COL_GAP_EM = parseFloat(getComputedStyle(document.querySelector(':root')).getPropertyValue('--column-gap-em')) || 1;
+    root.style.setProperty(`--column-gap-em-${(IS_VERTICAL) ? 'w' : 'h'}`, 0); // FULL
+    root.style.setProperty(`--column-gap-em-${(IS_VERTICAL) ? 'h' : 'w'}`, COL_GAP_EM); // SPLIT
+    console.log(`--block-size-base:${getComputedStyle(document.querySelector(':root')).getPropertyValue('--block-size-base')}`);
+    console.log(`--inline-size-base:${getComputedStyle(document.querySelector(':root')).getPropertyValue('--inline-size-base')}`);
+    console.log(`block-size:${getComputedStyle(document.querySelector('body')).getPropertyValue('block-size')}`);
+    console.log(`inline-size:${getComputedStyle(document.querySelector('body')).getPropertyValue('inline-size')}`);
+}
