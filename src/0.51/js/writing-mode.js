c3e2545300e6b2024213087114c668e3a6098c5a
ytyaru <ytyaru@outlook.jp>
2022-01-14 18:04:31 +0900

    0.51 現在画面の先頭要素を取得する（バグ。正しい値が取得できず縦横変更時に正しい現在ページ数をセットできない）

---
+++ src/0.51/js/writing-mode.js
@@ -0,0 +1,77 @@
+function saveWritingMode() {
+    const writingMode = document.querySelector('#writing-mode');
+    localStorage.setItem('writing-mode', getComputedStyle(document.querySelector(':root')).getPropertyValue('--writing-mode'));
+}
+function isVartical(value=null) { return ('vertical-rl' === (value || document.querySelector('#writing-mode').value)); }
+function initWritingMode() { // UI操作でフォントサイズを変更する
+    const root = document.querySelector(':root');
+    const writingMode = document.querySelector('#writing-mode');
+    function getButtonValue(value) { return (isVartical(value)) ? value : 'horizontal-tb'; }
+    function getButtonText(value) { return (isVartical(value)) ? '縦' : '横'; }
+    function getFontSizeBaseValue(value) { return `100v${(isVartical(value)) ? 'h' : 'w'}`; }
+    function switchWritingModeValue(value) { return (isVartical(value)) ? 'horizontal-tb' : 'vertical-rl';}
+    function getTextOrientationValue(writingMode) { return (isVartical(writingMode)) ? 'upright' : 'sideways'; }
+    function setValues(e, value) {
+        e.value = value;
+        e.innerHTML = getButtonText(value);
+        root.style.setProperty('--writing-mode', value);
+        root.style.setProperty('--text-orientation', getTextOrientationValue(value));
+        root.style.setProperty('--block-size-base', '100v' + ((isVartical(value)) ? 'w' : 'h'));
+        root.style.setProperty('--inline-size-base', '100v' + ((isVartical(value)) ? 'h' : 'w'));
+        console.log(`表記方向:${getButtonText(value)}`);
+    }
+    function loadWritingMode() {
+        const loadedValue = localStorage.getItem('WritingMode') || getComputedStyle(root).getPropertyValue('--writing-mode');
+        setValues(writingMode, loadedValue );
+    }
+    loadWritingMode();
+    function toggleWritingModeToHtml(e) { // CSSの変数をHTMLにセットする
+        const switchedWritingMode = switchWritingModeValue(getComputedStyle(root).getPropertyValue('--writing-mode'));
+        setValues(e.target, switchedWritingMode);
+    }
+    function setInputRangeWritingMode() {
+        for (const input of document.querySelectorAll('input[type="range"]')) {
+            if (isVartical()) {
+                input.setAttribute('style', '-webkit-appearance:slider-vertical; writing-mode: bt-lr;');
+                input.setAttribute('orient', 'vertical');
+            } else {
+                input.setAttribute('style', '');
+                input.setAttribute('orient', '');
+            }
+        }
+    }
+    writingMode.addEventListener('click', e => {
+//        function cssI(key) { return parseInt(getComputedStyle(document.querySelector(':root')).getPropertyValue(key)); }
+//        const oldHeadElement = document.querySelector(`main:not([hidden]) > p[page="${cssI('--page-index')}"]`);
+//        const oldHeadElement = getHeadElement(); // scroll-position-element.js WritingMode変更前に実行する
+        const a = getHeadElement(); // scroll-position-element.js WritingMode変更前に実行する
+        console.log(a);
+        const oldHeadElement = document.querySelector('#head-element');
+        console.log(oldHeadElement );
+
+        toggleWritingModeToHtml(e);
+        setInputRangeWritingMode();
+        setMaxColumns(); // columns.js
+        setMaxLineOfChars(); // MinFontSize.js
+        // 画面サイズはフォントサイズに依存しているため、２回計算する必要がある。１回で済むようにしたいのだが循環参照してしまう
+        calcScreenSize(e.target.value, document.querySelector('#column-count').value); // resize.js
+        setFontSizePixel(e.target.value, parseInt(document.querySelector('#line-of-chars').value), parseFloat(document.querySelector('#letter-spacing').value));
+        calcScreenSize(e.target.value, document.querySelector('#column-count').value); // resize.js
+        setFontSizePixel(e.target.value, parseInt(document.querySelector('#line-of-chars').value), parseFloat(document.querySelector('#letter-spacing').value));
+
+        breakPage();
+
+        oldHeadElement.scrollIntoView(true); // 変更前の先頭要素へ移動する
+//        breakPage();
+        // 現在ページ数は？　どうやって計算する？
+        const newHeadElement = getHeadElement(); // scroll-position-element.js WritingMode変更前に実行する
+        oldHeadElement.scrollIntoView(true);
+        const NOW_PAGE = newHeadElement.getAttribute('page'); // p要素が複数カラムにまたがっていたら先頭位置になる
+        document.querySelector(':root').style.setProperty('--page-index', NOW_PAGE);
+        /*
+        */
+    });
+    setInputRangeWritingMode();
+    return writingMode;
+}
+
