4505cfe49df220ebf95d398af5bc747dd042049a
ytyaru <ytyaru@outlook.jp>
2022-01-14 10:35:52 +0900

    0.45 block-start座標が増加する最初の要素をページの先頭として計算した（自作属性`page`を追加。break-page.js）

---
+++ src/0.45/js/break-page.js
@@ -0,0 +1,37 @@
+function breakPage() { // 本文表示領域(main要素)内を超過するところでbreak-before:columnする
+    function cssF(key) { return parseFloat(getComputedStyle(document.querySelector(':root')).getPropertyValue(key)); }
+    let page = 1; // ページ数
+    let page_pos = 0; // ページ座標
+    const IS_VERTICAL = ('vertical-rl' === document.querySelector('#writing-mode').value);
+    const MAIN_BLOCK_SIZE = cssF(((IS_VERTICAL) ? 'width' : 'height'), 'main:not([hidden])');
+    console.log(`breakPage MAIN_BLOCK_SIZE:${MAIN_BLOCK_SIZE}`);
+    for (const p of document.querySelectorAll('p')) {
+        const RECT = p.getBoundingClientRect();
+        console.log('RECT:', RECT);
+        const P_INLINE_START = RECT.left;
+        const P_BLOCK_START = (IS_VERTICAL) ? RECT.top : RECT.left; 
+        const P_PAGE_POS = P_BLOCK_START;
+        //p.setAttribute('page', `${page}`);
+        //page++;
+
+        p.removeAttribute('page');
+        if (page_pos < P_PAGE_POS) { // 次のページの先頭要素
+//            p.style.setProperty('break-before', 'all');
+            p.setAttribute('page', `${page}`);
+            page++;
+            page_pos = P_PAGE_POS;
+            console.log(`ページ追加:${page}, page_pos:${page_pos}`);
+        }
+        /*
+        */
+        /*
+//        const P_INLINE_START = (IS_VERTICAL) ? RECT.left : RECT.
+        if (MAIN_BLOCK_SIZE < P_INLINE_START) {
+            p.style.setProperty('break-before', 'all');
+            p.setAttribute('page', `${page}`);
+            page++;
+            console.log(`ページ追加:${page}`);
+        }
+        */
+    }
+}
