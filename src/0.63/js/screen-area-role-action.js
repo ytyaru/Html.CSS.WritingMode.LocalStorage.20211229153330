41b82cd5921e7e1a174cf0b3d5cc9b46a5d7075b
ytyaru <ytyaru@outlook.jp>
2022-01-16 16:00:25 +0900

    0.63 不要なファイル、関数、コメントアウトを削除した。

---
+++ src/0.63/js/screen-area-role-action.js
@@ -0,0 +1,41 @@
+function runScreenAreaRole(x, y) { // 画面エリアに応じた役割を実行する
+    function toggleDialog(dialog) { (dialog.open) ? dialog.close() : dialog.showModal(); document.body.style.cursor = 'auto'; }
+    switch (getScreenAreaRole(x, y)) {
+        case ROLE_PAGING_FORWARD: return nextPage();
+        case ROLE_PAGING_BACK: return prevPage();
+        case ROLE_PAGING_FIRST: return firstPage(); 
+        case ROLE_PAGING_LAST: return lastPage(); 
+        case ROLE_TOGGLE_MENU_SETTING: return toggleDialog(document.querySelector('#setting'));
+
+        case ROLE_PAGING: return; 
+        case ROLE_PAGING_SET: return; 
+        case ROLE_PAGING_AUTO: return; 
+        case ROLE_TOGGLE_MENU: return; 
+        case ROLE_TOGGLE_MENU_INDEX: return; 
+        case ROLE_TOGGLE_MENU_SETTING: return; 
+        case ROLE_TOGGLE_MENU_TOOLS: return; 
+        default: return;
+    }
+}
+function getCursorScreenAreaRole(x, y) { // 画面エリアの役割に応じたマウスカーソルを返す
+    const IS_VERTICAL = ('vertical-rl' === document.querySelector('#writing-mode').value);
+    switch (getScreenAreaRole(x, y)) {
+        case ROLE_PAGING_FORWARD: return (IS_VERTICAL) ? 'w-resize' : 'e-resize';
+        case ROLE_PAGING_BACK: return (IS_VERTICAL) ? 'e-resize' : 'w-resize';
+        case ROLE_TOGGLE_MENU_SETTING: return 'url("https://ytyaru.github.io/Html.CSS.WritingMode.LocalStorage.20211229153330/assets/image/setting_16x16.png"),auto';
+        /*
+        case ROLE_PAGING_FIRST: return ; 
+        case ROLE_PAGING_LAST: return ; 
+        case ROLE_TOGGLE_MENU_SETTING: return;
+
+        case ROLE_PAGING: return; 
+        case ROLE_PAGING_SET: return; 
+        case ROLE_PAGING_AUTO: return; 
+        case ROLE_TOGGLE_MENU: return; 
+        case ROLE_TOGGLE_MENU_INDEX: return; 
+        case ROLE_TOGGLE_MENU_SETTING: return; 
+        case ROLE_TOGGLE_MENU_TOOLS: return; 
+        */
+        default: return 'auto';
+    }
+}
