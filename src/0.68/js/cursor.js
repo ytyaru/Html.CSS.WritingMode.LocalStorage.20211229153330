3d1b232568b3d63101b7e32bd9508ff90d24cc2c
ytyaru <ytyaru@outlook.jp>
2022-01-20 09:37:17 +0900

    0.68 マウス位置修正。自作マウスは反応が遅いので使用を見送る。

--- src/0.68/js/cursor.js
+++ src/0.68/js/cursor.js
@@ -1,11 +1,20 @@
 function initCursor() {
-    function cssF(key, e) { return parseFloat(getComputedStyle(e).getPropertyValue(key)); }
-    const CURSOR = document.getElementById('cursor')
-    const width = cssF('width', CURSOR);
-    const height = cssF('height', CURSOR);
+//    function cssF(key, e) { return parseFloat(getComputedStyle(e).getPropertyValue(key)); }
+    function cssF(key, q=':root') { return parseFloat(getComputedStyle(document.querySelector(q)).getPropertyValue(key)); }
+//    const CURSOR = document.getElementById('cursor')
+    const width = cssF('width', '#cursor');
+    const height = cssF('height', '#cursor');
     window.addEventListener('mousemove', (e) => {
-        mouseX = e.pageX;
-        mouseY = e.pageY;
+//        const X = e.clientX - (width / 2);
+        const X = Math.floor(e.clientX - (width / 2));
+        const Y = Math.floor(e.clientY - (height / 2));
+//        const W = cssF('width', 'body');
+//        const H = cssF('height', 'body');
+        const W = document.body.clientWidth;
+        const H = document.documentElement.clientHeight;
+//        console.log(X,Y,W,H)
+        if (X < 0) { return; }
+        if (H < (e.clientY + (height / 2) + 1)) { return; }
         document.querySelector(':root').style.setProperty('--cursor-x', e.clientX - (width / 2));
         document.querySelector(':root').style.setProperty('--cursor-y', e.clientY - (height / 2));
         //console.log(e.pageX - (width / 2), e.pageY - (height / 2));
