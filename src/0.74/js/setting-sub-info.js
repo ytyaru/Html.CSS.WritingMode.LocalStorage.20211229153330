6bd9841848e9754fd448613dc165b29aad40a04a
ytyaru <ytyaru@outlook.jp>
2022-01-20 21:22:27 +0900

    0.74 補足情報がすべて非表示のとき余白の最小値を0にする（余白1.5未満のときに補足情報の表示がひとつでもONだと余白の最小値を1.5にする）

--- src/0.74/js/setting-sub-info.js
+++ src/0.74/js/setting-sub-info.js
@@ -8,18 +8,39 @@ function getSettingSubInfoElements() {
     return [CLOCK, ELAPSED_TIME, NOW_SECTION_HEADING, READ_RATE, PAGE_NUMBER, REMAINING_PAGES];
 }
 function initSettingSubInfo() { // 補足情報（ページヘッダ、フッタ）の表示ON/OFF
-    function loadStorage(INPUT) {
+    function loadFromStorage(INPUT) {
         const VALUE = ('0' === localStorage.getItem(`${INPUT.id}`)) ? 'hidden' : 'visible';
         document.querySelector(':root').style.setProperty(`--${INPUT.id}`, VALUE);
         INPUT.checked = ('visible' === VALUE);
     }
-    function addEvent(INPUT) {
+    function round(value, step=1.0) { // 指定したstep単位で丸める（今回は0.5単位で丸めたい）
+        var inv = 1.0 / step;
+        return Math.round(value * inv) / inv;
+    }
+    function establishMargin() { // マージンとの整合性をとる
+        const INPUTS = getSettingSubInfoElements();
+        const MARGIN = document.getElementById('margin');
+        // 補足情報がすべて非表示のとき、余白の最小値を0にする（1.5em未満にできる）
+        if (INPUTS.every(x=> !x.checked)) {
+            MARGIN.min = 0;
+            MARGIN.value = 0;
+            MARGIN.dispatchEvent(new Event('input'));
+        }
+        // 余白1.5em未満のとき、補足情報のいずれかが表示ONになったら、余白の最小値と値を1.5emにする
+        if (round(parseFloat(MARGIN.value), 0.5) <= 1.5 && INPUTS.some(x=> x.checked)) {
+            MARGIN.min = 1.5;
+            MARGIN.value = 1.5;
+            MARGIN.dispatchEvent(new Event('input'));
+        }
+    }
+    function addEvent(INPUT) { // UI操作イベントを実装する
         INPUT.addEventListener('change', (e)=>{
             document.querySelector(':root').style.setProperty(`--${e.target.id}`, (e.target.checked) ? 'visible' : 'hidden');
+            establishMargin();
         });
     }
     for (const INPUT of getSettingSubInfoElements()) {
-        loadStorage(INPUT);
+        loadFromStorage(INPUT);
         addEvent(INPUT);
     }
 }
