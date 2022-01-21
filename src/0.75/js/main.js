20b6a18393f543c99c8b2341fb34eb2f65f06a5d
ytyaru <ytyaru@outlook.jp>
2022-01-21 10:30:58 +0900

    0.75 補足情報の表示ON/OFFチェックボックスの表示位置を実際の表示位置とあわせた（縦書きのときは読了率が右、横書きのときは左など）

---
+++ src/0.75/js/main.js
@@ -0,0 +1,57 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    initFullScreen();
+    initWritingMode();
+    initColorScheme();
+    initColumns();
+    initLineOfChars();
+    initLineHeight();
+    initLetterSpacing();
+    initMargin();
+    initSettingSubInfo();
+    initPaging();
+    breakPage();
+    setNowSectionHeading();
+    setPageHeaderPosition();
+    setPosPageFooter();
+    calcReadRate();
+    initClock();
+    console.log("DOMContentLoaded");
+    console.log(`devicePixelRatio = ${window.devicePixelRatio}`);
+});
+window.addEventListener('load', (event) => {
+    console.log("load");
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log("beforeunload");
+    saveFullScreen();
+    saveColumns();
+    saveWritingMode();
+    saveLineOfChars();
+    saveLineHeight();
+    saveSettingSubInfo();
+    saveMargin();
+    saveLetterSpacing();
+    removeClock();
+});
+window.addEventListener("orientationchange", function () { // 画面向きに応じて最大字数／行を変更する
+    console.log("orientationchange");
+    setMaxColumns(); // columns.js
+    setMaxLineOfChars(); // resize.js
+    setFontSizePixel(document.querySelector('#writing-mode').value, parseInt(document.querySelector('#line-of-chars').value), parseFloat(document.querySelector('#letter-spacing').value)); // resize.js
+    breakPage();
+});
+window.addEventListener("resize", function (e) { // 全画面やリサイズ時に字／行の値を再計算する
+    console.log("resize");
+    setMaxColumns(); // columns.js
+    setMaxLineOfChars(); // resize.js
+    setFontSizePixel(document.querySelector('#writing-mode').value, parseInt(document.querySelector('#line-of-chars').value), parseFloat(document.querySelector('#letter-spacing').value)); // resize.js
+    breakPage();
+});
+// OSのダークモード変更に合わせる
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    const COLOR_SCHEME = document.getElementById('color-scheme');
+    COLOR_SCHEME.value = event.matches ? 'dark' : 'light';
+    COLOR_SCHEME.innerText = event.matches ? '🌙' : '☀';
+    document.querySelector(':root').style.setProperty('--color-scheme', COLOR_SCHEME.value)
+});
+
