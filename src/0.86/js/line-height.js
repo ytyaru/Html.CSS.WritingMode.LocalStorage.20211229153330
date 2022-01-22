aaa9d3665c18b1e2559ec04415bbae8455355d26
ytyaru <ytyaru@outlook.jp>
2022-01-22 17:13:00 +0900

    0.86 パフォーマンス改善（初回起動時16秒かかっていたが8秒にまで落とした（フォントサイズとページの計算呼出を１回だけ実行するようにした。content-visibility: auto;、contain: strict;した。））

---
+++ src/0.86/js/line-height.js
@@ -0,0 +1,26 @@
+function saveLineHeight() {
+    const lineHeight = document.querySelector('#line-height');
+    localStorage.setItem('line-height', lineHeight.value);
+}
+function initLineHeight() { // UI操作で行間を変更する
+    function getLineHeightFromCss() { // CSSの変数を取得する
+        const root = document.querySelector(':root');
+        const value = ('line-height' in localStorage) ? localStorage.getItem('line-height') : getComputedStyle(root).getPropertyValue('--line-height');
+        return [root, value];
+    }
+    const lineHeight = document.querySelector('#line-height');
+    const [root, value] = getLineHeightFromCss();
+    lineHeight.value = value;
+    function setLineHeightToHtml(value) { // CSSの変数をHTMLにセットする
+        root.style.setProperty('--line-height', `${value}`);
+        document.querySelector('#line-height-label').innerHTML = value * 100;
+        console.log(`${value}em行間`);
+    }
+    lineHeight.addEventListener('input', e => {
+        setLineHeightToHtml(e.target.value);
+        breakPage();
+    });
+    //lineHeight.dispatchEvent(new Event('input'));
+    setLineHeightToHtml(value);
+    return lineHeight;
+}
